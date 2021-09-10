393ec61fa3079fe6f8a2d15e91ba69454e67f03f
Ben Frengley <ben.frengley@gmail.com>
2021-09-11 01:24:14 +1200

    Day 13

--- src/main/kotlin/day13.kt
+++ src/main/kotlin/day13.kt
@@ -8,29 +8,45 @@ fun main(args: Array<String>) {
     val busList = busLine.split(',')
         .mapIndexedNotNull { i, bus ->
             bus.toLongOrNull()?.let { Pair(i, it) }
-        }.toList().also(::println)
+        }.toList()
     val busIds = busList.map { (_, b) -> b }
 
     print("Part 1: ")
     val earliestBus = busIds.minByOrNull { it - (earliestTime % it) }!!
     println(earliestBus * (earliestBus - (earliestTime % earliestBus)))
 
-//    print("Part 2: ")
-//    val idsFromMin = busIds.sorted()
-//    val busTimes = sequence {
-//        for (i in earliestTime..Long.MAX_VALUE) {
-//            if (idsFromMin.any { i % it == 0L }) {
-//                yield(i)
-//            }
-//        }
-//    }
-//    busTimes.windowed(busIds.size).find {
-//        it.zip(busList).windowed(2).parallelStream().allMatch { pair ->
-//            val (t1, busPair1) = pair.first()
-//            val (i1, bus1) = busPair1
-//            val (t2, busPair2) = pair.last()
-//            val (i2, bus2) = busPair2
-//            (t2 - t1) == (i2 - i1).toLong() && t1 % bus1 == 0L && t2 % bus2 == 0L
-//        }
-//    }?.let(::println)
+    print("Part 2: ")
+    val res = gaussianCRT(busIds, busList.map { it.second - it.first })
+    println(res)
+}
+
+// https://shainer.github.io/crypto/math/2017/10/22/chinese-remainder-theorem.html
+fun gaussianCRT(ns: List<Long>, vs: List<Long>): Long {
+    var res = 0L
+    val prod = ns.fold(1L) { acc, n -> acc * n }
+
+    for ((n, v) in ns.zip(vs)) {
+        val p = prod / n
+        res += v * p * multInv(p, n)
+    }
+    return res % prod
+}
+
+fun multInv(a: Long, b: Long): Long {
+    if (b == 1L) return 1L
+    var aa = a
+    var bb = b
+    var x0 = 0L
+    var x1 = 1L
+    while (aa > 1L) {
+        val q = aa / bb
+        var t = bb
+        bb = aa % bb
+        aa = t
+        t = x0
+        x0 = x1 - q * x0
+        x1 = t
+    }
+    if (x1 < 0L) x1 += b
+    return x1
 }
