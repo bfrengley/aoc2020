8c87bd974ad900487cf922a117bb8f9442b5eb83
Ben Frengley <ben.frengley@gmail.com>
2021-09-12 01:13:30 +1200

    Day 14 complete

--- src/main/kotlin/day14.kt
+++ src/main/kotlin/day14.kt
@@ -2,31 +2,52 @@ package aoc2020.day14
 
 import aoc2020.util.loadTextResource
 
+val toBinaryString = java.lang.Long::toBinaryString
+
 fun main(args: Array<String>) {
-    val lines = loadTextResource("/day14.txt").lines()
+    val instrs = loadTextResource("/day14.txt").lines().map(Instr::parse)
 
-
+    part1(instrs)
+    part2(instrs)
 }
 
-fun part1(lines: Iterator<String>) {
+fun part1(instrs: List<Instr>) {
     var mask = Bitmask.parse("")
-    val mem = mutableListOf<Long>()
+    val mem = mutableMapOf<Long, Long>()
 
-    for (line in lines) {
-        when (val instr = Instr.parse(line)) {
+    for (instr in instrs) {
+        when (instr) {
             is Instr.Mask -> mask = instr.mask
             is Instr.Set -> {
-                if (mem.size < instr.addr) {
+                mem[instr.addr] = mask(instr.value)
+            }
+        }
+    }
 
+    println("Part 1: ${mem.values.sum()}")
+}
+
+fun part2(instrs: List<Instr>) {
+    var mask = Bitmask.parse("")
+    val mem = mutableMapOf<Long, Long>()
+
+    for (instr in instrs) {
+        when (instr) {
+            is Instr.Mask -> mask = instr.mask
+            is Instr.Set -> {
+                for (addr in mask.maskAddress(instr.addr)) {
+                    mem[addr] = instr.value
                 }
             }
         }
     }
+
+    println("Part 2: ${mem.values.sum()}")
 }
 
 sealed class Instr {
-    class Mask(mask: Bitmask) : Instr()
-    class Set(addr: Int, value: Long) : Instr()
+    class Mask(val mask: Bitmask) : Instr()
+    class Set(val addr: Long, val value: Long) : Instr()
 
     companion object {
         fun parse(str: String): Instr {
@@ -34,7 +55,7 @@ sealed class Instr {
             return if (l.startsWith("mask")) {
                 Mask(Bitmask.parse(r.trim()))
             } else if (l.startsWith("mem")) {
-                val addr = l.substring(l.indexOf('['), l.indexOf(']')).toInt()
+                val addr = l.substring(l.indexOf('[') + 1, l.indexOf(']')).toLong()
                 val value = r.trim().toLong()
                 Set(addr, value)
             } else {
@@ -44,23 +65,42 @@ sealed class Instr {
     }
 }
 
-data class Bitmask(val setmask: Long, val clearmask: Long) {
+data class Bitmask(val setmask: Long, val clearmask: Long, val floatmask: Long) {
+    private val floatmaskBits = toBinaryString(floatmask).reversed()
+
     operator fun invoke(n: Long) = (n or setmask) and clearmask
+
+    fun maskAddress(addr: Long) = ((addr or setmask) and floatmask.inv()).let { baseAddr ->
+        floatmaskBits.foldIndexed(listOf(baseAddr)) { i, masks, b ->
+            if (b == '1') masks + masks.map { it or (1L shl i) } else masks
+        }
+    }
+
+    override fun toString(): String {
+        return "Bitmask(setmask=${
+            toBinaryString(setmask)
+        }, clearmask=${
+            toBinaryString(clearmask.inv())
+        }, floatmask=${
+            toBinaryString(floatmask)
+        })"
+    }
 
     companion object {
         fun parse(str: String): Bitmask {
             var setmask = 0L
             var clearmask = 0L.inv()
+            var floatmask = 0L
 
             for ((i, c) in str.reversed().withIndex()) {
                 when (c) {
                     '1' -> setmask = setmask or (1L shl i)
                     '0' -> clearmask = clearmask xor (1L shl i)
-                    else -> {}
+                    else -> floatmask = floatmask or (1L shl i)
                 }
             }
 
-            return Bitmask(setmask, clearmask)
+            return Bitmask(setmask, clearmask, floatmask)
         }
     }
 }
